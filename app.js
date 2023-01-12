916ea7bc7ed9adb42d2623ef6c4613096b4a1e7c
facupalopoli <facupalopoli@gmail.com>
2023-01-12 03:46:57 -0300

    version2.3

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-let idDispo = 0
-let idGasto = null
-let idMonto = 0
+"use strict"
+
+let disponible
+let idGasto
+let idMonto 
 let idLista = document.getElementById('idLista') //asignacion de la lista
-let montoIngresos = document.getElementById('montoIngresos') //
+let montoIngresos = document.getElementById('montoIngresos') 
 let montoGastos = document.getElementById('montoGastos')
 let montoDisponible = document.getElementById('montoDisponible')
-let agregarGasto = document.getElementById('agregarGasto')
+
+
 
 let diaNoche = document.getElementById('diaNoche')
+
 diaNoche.addEventListener('click', () => {   //funcion que escucha el click y cambia la clase al body
     document.body.classList.toggle('dark')
     diaNoche.classList.toggle('active')
@@ -15,38 +19,48 @@ diaNoche.addEventListener('click', () => {   //funcion que escucha el click y ca
 
 
 
-function agregarDisponible(e){      //funcion que escucha una tecla cualquier y despues pregunta si es el Enter
-    if(e.keyCode === 13){
-        e.preventDefault()
-        idDispo = parseFloat(document.getElementById('idDispo').value)
-        montoIngresos.innerText = `$${idDispo}`
-        montoDisponible.innerText = `$${idDispo-sumaGastos}`
-        if((idDispo-sumaGastos) >= 0){
-            montoDisponible.style.color=`#008000`
-        }else{
-            montoDisponible.style.color=`#D23D03`
-        }
-    }
+
+let inputDispo = document.getElementById('inputDispo')
+
+const agregar = () => { 
+    disponible = parseFloat(inputDispo.value)
+    montoIngresos.innerText = `$${disponible}`
+    montoDisponible.innerText = `$${disponible-sumaGastos}`
+    disponible-sumaGastos >= 0 ? montoDisponible.style.color=`#008000` : montoDisponible.style.color=`#D23D03`
 }
 
+inputDispo.addEventListener('focusout',agregar)
+
+
+
+let agregarGasto = document.getElementById('agregarGasto')
 let sumaGastos = 0
+
 const gasto = () => { 
     idGasto = document.getElementById('idGasto').value
     idMonto = parseFloat(document.getElementById('idMonto').value)
-    sumaGastos += idMonto
-    idLista.innerHTML += `<li>
-                            <div>
-                                <i class="bi bi-cash-coin iconBillete"></i>
-                                <span>${idGasto}</span>
-                            </div>
-                            <div>
-                                <span>$${idMonto}</span>
-                                <i onclick="eliminarGasto()" class="bi bi-trash3 iconTacho"></i>
-                            </div>
-                          </li>`
-    montoGastos.innerText = `$${sumaGastos}`
-    montoDisponible.innerText = `$${idDispo-sumaGastos}`
-    if((idDispo-sumaGastos) < 0){
+    if(!disponible){
+        alert('Primero ingrese su dinero disponible')
+    }else{
+        if(idMonto > 0 && idGasto != ""){
+        sumaGastos += idMonto
+        idLista.innerHTML += `<li>
+                                <div>
+                                    <i class="bi bi-cash-coin iconBillete"></i>
+                                    <span>${idGasto}</span>
+                                </div>
+                                <div>
+                                    <span>$${idMonto}</span>
+                                    <i id="eliminarGasto" class="bi bi-trash3 iconTacho"></i>
+                                </div>
+                            </li>`
+        montoGastos.innerText = `$${sumaGastos}`
+        montoDisponible.innerText = `$${disponible-sumaGastos}`
+        }else{
+            alert('Detalle el gasto correctamente')
+        }    
+    }
+    if((disponible-sumaGastos) < 0){
         montoDisponible.style.color=`#D23D03`
     }
 }
@@ -55,30 +69,10 @@ agregarGasto.addEventListener('click',gasto)
 
 
 
+let eliminarGasto = document.getElementById('eliminarGasto')
 
-
-/* function agregarGasto(){
-    idGasto = document.getElementById('idGasto').value
-    idMonto = parseFloat(document.getElementById('idMonto').value)
-    sumaGastos += idMonto
-    idLista.innerHTML += `<li>
-                            <div>
-                                <i class="bi bi-cash-coin iconBillete"></i>
-                                <span>${idGasto}</span>
-                            </div>
-                            <div>
-                                <span>$${idMonto}</span>
-                                <i onclick="eliminarGasto()" class="bi bi-trash3 iconTacho"></i>
-                            </div>
-                          </li>`
-    montoGastos.innerText = `$${sumaGastos}`
-    montoDisponible.innerText = `$${idDispo-sumaGastos}`
-    if((idDispo-sumaGastos) < 0){
-        montoDisponible.style.color=`#D23D03`
-    }
-} */
-
-function eliminarGasto(){
+const eliminar = () => {
+    console.log('asdas')
     idLista.innerHTML -= `<li>
                             <div>
                                 <i class="bi bi-cash-coin iconBillete"></i>
@@ -86,7 +80,11 @@ function eliminarGasto(){
                             </div>
                             <div>
                                 <span>$${idMonto}</span>
-                                <i onclick="eliminarGasto()" class="bi bi-trash3 iconTacho"></i>
+                                <i id="eliminarGasto" class="bi bi-trash3 iconTacho"></i>
                             </div>
                           </li>`
+}
+
+if (eliminarGasto){
+eliminarGasto.addEventListener('click',eliminar)
 }
