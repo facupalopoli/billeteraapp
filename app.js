c186690be35e56b90ef3042e306f7bb8f2ae527f
facupalopoli <facupalopoli@gmail.com>
2023-01-12 00:24:30 -0300

    version2.1

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 let idDispo = 0
 let idGasto = null
 let idMonto = 0
-let idLista = document.getElementById('idLista')
-let montoIngresos = document.getElementById('montoIngresos')
+let idLista = document.getElementById('idLista') //asignacion de la lista
+let montoIngresos = document.getElementById('montoIngresos') //
 let montoGastos = document.getElementById('montoGastos')
 let montoDisponible = document.getElementById('montoDisponible')
 
 let diaNoche = document.getElementById('diaNoche')
-diaNoche.addEventListener('click', () => {
+diaNoche.addEventListener('click', () => {   //funcion que escucha el click y cambia la clase al body
     document.body.classList.toggle('dark')
     diaNoche.classList.toggle('active')
 })
 
 
 
-function agregarDisponible(e){
+function agregarDisponible(e){      //funcion que escucha una tecla cualquier y despues pregunta si es el Enter
     if(e.keyCode === 13){
         e.preventDefault()
         idDispo = parseFloat(document.getElementById('idDispo').value)
         montoIngresos.innerText = `$${idDispo}`
-        montoDisponible.innerText = `$${idDispo}`
+        montoDisponible.innerText = `$${idDispo-sumaGastos}`
+        if((idDispo-sumaGastos) >= 0){
+            montoDisponible.style.color=`#008000`
+        }else{
+            montoDisponible.style.color=`#D23D03`
+        }
     }
 }
 
